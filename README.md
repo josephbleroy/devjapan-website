bc86b6612e3043b4fe398a058d49c2eb9121f9e4
Joseph LeRoy <jleroy1@fordham.edu>
2016-10-13 13:57:15 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# devjapan-website
+
+This is the repository for the [Dev Japan] website!
+
+## Development
+
+### Requirements
+
+1. Have [node.js] installed
+2. [Fork] and clone the repository to your machine
+3. `npm install` to install the relevant node packages
+
+### Making changes
+
+Make your changes in the relevant files within the `src` directory.
+Then, to compile your changes do `gulp build`. Check out the compiled files
+in the git ignored `build` directory.
+
+If you want to make multiple changes and have the files rebuilt on every
+change that you do, use `gulp dev`.
+
+
+[devjapan]: https://devjapan.jp
+[node.js]: https://nodejs.org
+[Fork]: https://help.github.com/articles/fork-a-repo/
---
+++ gulpfile.babel.js
@@ -0,0 +1,74 @@
+import {src, dest, watch, parallel, series} from 'gulp';
+import del from 'del';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import atImport from 'postcss-import';
+import minify from 'cssnano';
+import simpleVars from 'postcss-simple-vars';
+import svgo from 'gulp-svgo';
+import imagemin from 'gulp-imagemin';
+import pixrem from 'pixrem';
+import rev from 'gulp-rev';
+import revReplace from 'gulp-rev-replace';
+
+// Sources
+const SRC_DIR = 'src';
+const BUILD_DIR = 'build';
+const CSS_GLOB = `${SRC_DIR}/**/*.css`;
+const IMG_GLOB = `${SRC_DIR}/images/**/*`;
+const MISC_GLOB = [`${SRC_DIR}/**/*`, `!${CSS_GLOB}`, `!${IMG_GLOB}`];
+// Clean task
+export function clean() {
+  return del([BUILD_DIR]);
+}
+
+// CSS
+const processors = [
+  atImport,
+  simpleVars,
+  autoprefixer({browsers: ['last 2 versions']}),
+  pixrem({rootValue: 10})
+];
+export function styles() {
+  return src(CSS_GLOB)
+    .pipe(postcss(processors))
+    .pipe(postcss([minify]))
+    .pipe(rev())
+    .pipe(dest(BUILD_DIR))
+    .pipe(rev.manifest())
+    .pipe(dest(BUILD_DIR));
+}
+
+// Images
+export function images() {
+  return src(IMG_GLOB, {base: SRC_DIR})
+    .pipe(svgo())
+    .pipe(imagemin())
+    .pipe(dest(BUILD_DIR));
+}
+
+// Pipe other files
+export function misc() {
+  return src(MISC_GLOB)
+    .pipe(dest(BUILD_DIR));
+}
+
+export function revAssets() {
+  let manifest = src('**/rev-manifest.json');
+  return src('**/*.html', {base: SRC_DIR})
+    .pipe(revReplace({manifest: manifest}) )
+    .pipe(dest(BUILD_DIR));
+}
+
+function watchSrc() {
+  watch(CSS_GLOB, styles);
+  watch(IMG_GLOB, images);
+  watch(MISC_GLOB, misc);
+  watch(['**/rev-manifest.json'], revAssets);
+};
+
+const mainTasks = parallel(styles, images, misc);
+export const build = series(clean, mainTasks, revAssets);
+export const dev = series(clean, mainTasks, revAssets, watchSrc);
+
+export default build;
